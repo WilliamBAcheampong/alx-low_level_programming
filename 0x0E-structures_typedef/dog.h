d76f677f18b05c897093856f116e05e13e342bab
root <boachieawilliam@gmail.com>
2023-08-14 16:39:55 -0700

    0x0E. C - Structures, typedef updated

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -19,10 +19,10 @@ struct dog
  */
 typedef struct dog dog_f;
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
+void init_dog(struct dog *k, char *name, float age, char *owner);
+void print_dog(struct dog *k);
 dog_f *new_dog(char *name, float age, char *owner);
-void free_dog(dog_f *d);
+void free_dog(dog_f *k);
 char *_strcpy(char *dest, char *src);
 int _strlen(char *s);
 
