ec58645916a87f91f75734827e1716f0da69286e
root <boachieawilliam@gmail.com>
2023-08-24 17:03:23 -0700

    0x12. C - Singly linked lists

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -10,14 +10,13 @@
  */
 void free_list(list_t *head)
 {
-	list_t *tempor;
+	list_t *temp;
 
 	while (head)
 	{
-		tempor = head->next;
+		temp = head->next;
 		free(head->str);
 		free(head);
-		head = tempor
-;
+		head = temp;
 	}
 }
