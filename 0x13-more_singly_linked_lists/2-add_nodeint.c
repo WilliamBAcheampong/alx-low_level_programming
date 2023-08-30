ccaf17895fb6a88e622360f6b2e8771abf768411
root <boachieawilliam@gmail.com>
2023-08-29 18:55:01 -0700

    0x13. C - More singly linked lists

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -11,7 +11,7 @@ listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new1;
 
-	new = malloc(sizeof(listint_t));
+	new1 = malloc(sizeof(listint_t));
 	if (!new1)
 		return (NULL);
 
