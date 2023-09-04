797d2ce33be8faccc888d2242a69cd6c244e96fe
root <boachieawilliam@gmail.com>
2023-09-04 13:56:50 -0700

    0x15. C - File I/O

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -3,10 +3,10 @@
 
 /**
  * read_textfile- Read text file print to STDOUT.
- * @letters: number of letters to be read,
- * @filename: text file being read.
+ * @filename: text file being read
+ * @letters: number of letters to be read
  * Return: w- actual number of bytes read and printed
- * 0 when function fails or filename is NULL.
+ *        0 when function fails or filename is NULL.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,15 +1,13 @@
 #include "main.h"
-
-#include <stdlib.h>
-
 #include <stdio.h>
+#include <stdlib.h>
 
 char *create_buffer(char *file);
 void close_file(int fd);
+
 /**
  * create_buffer - Allocates 1024 bytes for a buffer.
- *
- * @file: The name of the file buffer that is storing chars
+ * @file: The name of the file buffer is storing chars for.
  *
  * Return: A pointer to the newly-allocated buffer.
  */
@@ -48,8 +46,8 @@ void close_file(int fd)
 
 /**
  * main - Copies the contents of a file to another file.
-* @argv: An array of pointers to the arguments.
  * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
  *
  * Return: 0 on success.
  *
