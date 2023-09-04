38c26ed47af13f85b7d2ea22d4a91c6014a69584
root <boachieawilliam@gmail.com>
2023-09-04 14:05:23 -0700

    0x15. C - File I/O

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,13 +1,15 @@
 #include "main.h"
-#include <stdio.h>
+
 #include <stdlib.h>
+
+#include <stdio.h>
 
 char *create_buffer(char *file);
 void close_file(int fd);
-
 /**
  * create_buffer - Allocates 1024 bytes for a buffer.
- * @file: The name of the file buffer is storing chars for.
+ *
+ * @file: The name of the file buffer that is storing chars
  *
  * Return: A pointer to the newly-allocated buffer.
  */
@@ -46,8 +48,8 @@ void close_file(int fd)
 
 /**
  * main - Copies the contents of a file to another file.
+* @argv: An array of pointers to the arguments.
  * @argc: The number of arguments supplied to the program.
- * @argv: An array of pointers to the arguments.
  *
  * Return: 0 on success.
  *
