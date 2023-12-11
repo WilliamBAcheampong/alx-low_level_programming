9f410e847c15eae89e6b94596972c403b4758f46
root <boachieawilliam@gmail.com>
2023-12-11 07:07:46 -0800

    updated

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-
-echo 'void make_me_win(void) __attribute__((constructor)); void make_me_win(void) { printf("Please make me win!\n"); }' > libwin.c
-gcc -Wall -fPIC -shared -o libwin.so libwin.c -ldl
-export LD_PRELOAD=./libwin.so
-./gm 9 8 10 24 75 9
+wget -P .. https://raw.githubusercontent.com/monoprosito/holbertonschool-low_level_programming/master/0x18-dynamic_libraries/libgiga.so
+export LD_PRELOAD="$PWD/../libgiga.so"
